8bfde64b20e16f0a2f64a740a5183f769dc7566f
fengpei0114 <feng_pei0114@163.com>
2020-10-27 23:32:42 +0800

    finish connect with BackEnd and show grouplist

--- src/App/App.js
+++ src/App/App.js
@@ -6,49 +6,7 @@ class App extends Component {
     super(props);
     this.state = {
       mytext: [],
-      group: [{
-        id:1,
-        users:[
-          {
-            id:1,
-            name:"成吉思汗"
-          },
-          {
-            id:2,
-            name:"鲁班七号"
-          },{
-            id:3,
-            name:"太乙真人"
-          }
-        ]
-      },{
-        id:2,
-        users:[
-          {
-            id:4,
-            name:"钟无艳"
-          },
-          {
-            id:5,
-            name:"花木兰"
-          },{
-            id:6,
-            name:"雅典娜"
-          }
-        ]
-      },{
-        id:3,
-        users:[
-          {
-            id:7,
-            name:"芈月"
-          },
-          {
-            id:8,
-            name:"刘禅"
-          }
-        ]
-      }]
+      group: [],
     }
     this.getData();
   }
@@ -74,21 +32,45 @@ class App extends Component {
     )
   }
 
+  sep_group = () => {
+    const myHeaders = new Headers({
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'text/plain'
+    });
+    fetch('http://localhost:8080/grouplist', {
+      method: 'GET',
+      headers: myHeaders,
+      mode: 'cors',
+    }).then(res => res.json()).then(
+      data => {
+        console.log(data);
+        this.setState({
+          group:[]
+        })
+        data.map((key) => {
+          this.setState({
+            group: [...this.state.group, key]
+          })
+        })
+      }
+    )
+  }
+
   render() {
     return (
       <>
-      <div className = "spa_div">
-        <h2 className = "spa_title">
+      <div className = "sep_div">
+        <h2 className = "sep_title">
           分组列表
         </h2>
-        <button className="spa_button">分组学员</button>
+        <button className="sep_button" onClick={this.sep_group}>分组学员</button>
         
       </div>
         {
           this.state.group.map(key => {
             return (
               <div className = "group_div">
-                <div className = "group_title"><p>{key.id}组</p></div>
+                <div className = "group_title"><p>{key.id + 1}组</p></div>
                 <div className = "group_number">
                 {
                   key.users.map(user => {
